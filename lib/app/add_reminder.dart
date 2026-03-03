cc4f6295eeba8d92380065bda6be256d8baece5e
imran-saleh11 <s.imran.work42@gmail.com>
2026-03-03 12:01:59 +0600

    Working development environment

---
+++ lib/app/add_reminder.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddReminderScreen extends StatelessWidget {
+  const AddReminderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/app_navigator.dart
+++ lib/app_navigator.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:remedic/app/home_screen.dart';
+import 'package:remedic/app/history_screen.dart';
+import 'package:remedic/app/add_reminder.dart';
+import 'package:remedic/app/settings_screen.dart';
+import 'package:remedic/app/profile_screen.dart';
 
-class AppNavigator extends StatelessWidget {
+class AppNavigator extends StatefulWidget {
   const AppNavigator({super.key});
 
   @override
+  State<AppNavigator> createState() => _AppNavigatorState();
+}
+
+class _AppNavigatorState extends State<AppNavigator> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    HistoryScreen(),
+    AddReminderScreen(),
+    SettingsScreen(),
+    ProfileScreen()
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    //body: _screens[_selectedIndex],
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        bottomNavigationBar: TabBar(
+          labelPadding: EdgeInsetsGeometry.directional(bottom: 12),
+          dividerColor: Colors.transparent,
+          indicatorColor: Colors.black,
+          tabs: [
+            Tab(icon: Icon(Icons.directions_car)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+            Tab(icon: Icon(Icons.directions_boat)),
+          ],
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+            Icon(Icons.directions_boat),
+          ],
+        ),
+      ),
+    );
   }
 }
